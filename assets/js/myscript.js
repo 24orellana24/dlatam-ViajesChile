6a45edf8d60ece517a48775ef2dca94ff2d37c9d
24orelllana24 <24orellana24@gmail.com>
2021-10-01 20:32:13 -0300

    mi primer commit

---
+++ assets/js/myscript.js
@@ -0,0 +1,5 @@
+// Código que se debe agrgar de acuerdo a lo documentación de bootstrap para los tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
